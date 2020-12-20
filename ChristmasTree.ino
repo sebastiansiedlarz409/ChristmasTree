0b584bae38a25b00cb474b8fac21807eb60e16a5
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2020-12-20 15:37:45 +0100

    Move to object

--- ChristmasTree.ino
+++ ChristmasTree.ino
@@ -1,214 +1,236 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN        6
-#define NUMPIXELS 30
+class LedBar{
+  public:
+    int PIN;
+    int LedCount;
+    Adafruit_NeoPixel* pixels;
+    LedBar(int pin, int ledCount){
+      PIN = pin;
+      LedCount = ledCount;
+      pixels = new Adafruit_NeoPixel(LedCount, PIN, NEO_GRB + NEO_KHZ800);
+    }
+    void Init();
+    void Clear();
+    void StepOne();
+    void StepTwo();
+    void StepThree();
+    void StepFour();
+};
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-void setup() {
-  pixels.begin();
+void LedBar::Init(){
+  pixels->begin();
 }
 
-void stepOne(){
-  for(int i=0; i<NUMPIXELS; i++) {
+void LedBar::Clear(){
+  pixels->clear();
+}
+
+void LedBar::StepOne(){
+  for(int i=0; i<LedCount; i++) {
     if(i % 6 == 0){
-      pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      pixels->setPixelColor(i, pixels->Color(255, 0, 0));
     }
     if(i % 6 == 1){
-      pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+      pixels->setPixelColor(i, pixels->Color(0, 255, 0));
     }
     if(i % 6 == 2){
-      pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+      pixels->setPixelColor(i, pixels->Color(0, 0, 255));
     }
     if(i % 6 == 3){
-      pixels.setPixelColor(i, pixels.Color(0, 255, 255));
+      pixels->setPixelColor(i, pixels->Color(0, 255, 255));
     }
     if(i % 6 == 4){
-      pixels.setPixelColor(i, pixels.Color(255, 0, 255));
+      pixels->setPixelColor(i, pixels->Color(255, 255, 0));
     }
     if(i % 6 == 5){
-      pixels.setPixelColor(i, pixels.Color(255, 255, 0));
+      pixels->setPixelColor(i, pixels->Color(255, 0, 255));
     }
-    pixels.show();
+    pixels->show();
     delay(60);
   }
   
   delay(500);
 
-  for(int i=NUMPIXELS-1; i>=0; i--) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
+  for(int i=LedCount-1; i>=0; i--) {
+    pixels->setPixelColor(i, pixels->Color(0, 0, 0));
+    pixels->show();
     delay(60);
   }
 }
 
-void stepTwo(){
+void LedBar::StepTwo(){
   for(int i=0; i<6; i++) {
     if(i % 6 == 0){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(255, 0, 0));
     }
     if(i % 6 == 1){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(0, 255, 0));
     }
     if(i % 6 == 2){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(0, 0, 255));
     }
     if(i % 6 == 3){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(0, 255, 255));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(0, 255, 255));
     }
     if(i % 6 == 4){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(255, 0, 255));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(255, 0, 255));
     }
     if(i % 6 == 5){
-      for(int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(255, 255, 0));
+      for(int i=0; i<LedCount; i++)
+        pixels->setPixelColor(i, pixels->Color(255, 255, 0));
     }
-    pixels.show();
+    pixels->show();
     delay(600);
   }
 }
 
-void stepThree(){
+void LedBar::StepThree(){
   //FORWARD
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(10, 0, 0));
-    pixels.setPixelColor(i, pixels.Color(100, 0, 0));
-    pixels.setPixelColor(i+1, pixels.Color(255, 0, 0));
+    pixels->setPixelColor(i-1, pixels->Color(10, 0, 0));
+    pixels->setPixelColor(i, pixels->Color(100, 0, 0));
+    pixels->setPixelColor(i+1, pixels->Color(255, 0, 0));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(0, 0, 10));
-    pixels.setPixelColor(i, pixels.Color(0, 0, 100));
-    pixels.setPixelColor(i+1, pixels.Color(0, 0, 255));
+    pixels->setPixelColor(i-1, pixels->Color(0, 0, 10));
+    pixels->setPixelColor(i, pixels->Color(0, 0, 100));
+    pixels->setPixelColor(i+1, pixels->Color(0, 0, 255));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(0, 10, 0));
-    pixels.setPixelColor(i, pixels.Color(0, 100, 0));
-    pixels.setPixelColor(i+1, pixels.Color(0, 255, 0));
+    pixels->setPixelColor(i-1, pixels->Color(0, 10, 0));
+    pixels->setPixelColor(i, pixels->Color(0, 100, 0));
+    pixels->setPixelColor(i+1, pixels->Color(0, 255, 0));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(255, 251, 0));
-    pixels.setPixelColor(i, pixels.Color(255, 251, 0));
-    pixels.setPixelColor(i+1, pixels.Color(255, 251, 0));
+    pixels->setPixelColor(i-1, pixels->Color(255, 251, 0));
+    pixels->setPixelColor(i, pixels->Color(255, 251, 0));
+    pixels->setPixelColor(i+1, pixels->Color(255, 251, 0));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(0, 10, 10));
-    pixels.setPixelColor(i, pixels.Color(0, 100, 100));
-    pixels.setPixelColor(i+1, pixels.Color(0, 255, 255));
+    pixels->setPixelColor(i-1, pixels->Color(0, 10, 10));
+    pixels->setPixelColor(i, pixels->Color(0, 100, 100));
+    pixels->setPixelColor(i+1, pixels->Color(0, 255, 255));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(10, 10, 0));
-    pixels.setPixelColor(i, pixels.Color(100, 100, 0));
-    pixels.setPixelColor(i+1, pixels.Color(255, 255, 0));
+    pixels->setPixelColor(i-1, pixels->Color(10, 10, 0));
+    pixels->setPixelColor(i, pixels->Color(100, 100, 0));
+    pixels->setPixelColor(i+1, pixels->Color(255, 255, 0));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(10,0, 10));
-    pixels.setPixelColor(i, pixels.Color(100,0, 100));
-    pixels.setPixelColor(i+1, pixels.Color(255,0, 255));
+    pixels->setPixelColor(i-1, pixels->Color(10,0, 10));
+    pixels->setPixelColor(i, pixels->Color(100,0, 100));
+    pixels->setPixelColor(i+1, pixels->Color(255,0, 255));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
-  for(int i=0; i<NUMPIXELS-1; i++) {
+  for(int i=0; i<LedCount-1; i++) {
     
-    pixels.setPixelColor(i-1, pixels.Color(245, 5, 173));
-    pixels.setPixelColor(i, pixels.Color(245, 5, 173));
-    pixels.setPixelColor(i+1, pixels.Color(245, 5, 173));
+    pixels->setPixelColor(i-1, pixels->Color(245, 5, 173));
+    pixels->setPixelColor(i, pixels->Color(245, 5, 173));
+    pixels->setPixelColor(i+1, pixels->Color(245, 5, 173));
     
-    pixels.show();
+    pixels->show();
     delay(60);
-    pixels.clear();
+    pixels->clear();
   }
 
-  pixels.clear();
+  pixels->clear();
 
   //FORWARD END
 
 }
 
-void stepFour(){
+void LedBar::StepFour(){
   for(int k = 0; k<10; k++){
     int r = (int)random(0, 255);
     int g = (int)random(0, 255);
     int b = (int)random(0, 255);
-    for(int i=NUMPIXELS/2,j = NUMPIXELS/2-1; i<NUMPIXELS; i++, j--) {
+    for(int i=LedCount/2,j = LedCount/2-1; i<LedCount; i++, j--) {
     
-      pixels.setPixelColor(j, pixels.Color(r,g,b));
-      pixels.setPixelColor(i, pixels.Color(r,g,b));
+      pixels->setPixelColor(j, pixels->Color(r,g,b));
+      pixels->setPixelColor(i, pixels->Color(r,g,b));
     
-      pixels.show();
+      pixels->show();
       delay(60);
-      pixels.clear();
+      pixels->clear();
     }
-
     delay(100);
   }
 }
 
+LedBar bottom(6, 30);
+
+void setup() {
+  bottom.Init();
+}
+
 void loop() {
-  pixels.clear();
+  bottom.Clear();
+  
+  bottom.StepOne();
 
-  stepOne();
+  bottom.StepTwo();
 
-  stepTwo();
+  bottom.StepThree();
 
-  stepThree();
-
-  stepFour();
+  bottom.StepFour();
 
 }
